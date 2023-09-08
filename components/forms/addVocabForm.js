4fda44efdb96871df8e81933515136580e625f06
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2023-09-07 19:48:09 -0500

    updated logout button, forms and dombuilder and navbar

--- components/forms/addVocabForm.js
+++ components/forms/addVocabForm.js
@@ -6,22 +6,18 @@ const addVocabForm = (uid, obj = {}) => {
   const domString = `
     <form id="${obj.firebaseKey ? `update-vocab--${obj.firebaseKey}` : 'submit-vocab'}" class="mb-4">
       <div class="form-group">
-        <label for="image">First Name</label>
-        <input type="text" class="form-control" id="first_name" placeholder="First Name" value="${obj.first_name || ''}" required>
+        <label for="image">Title</label>
+        <input type="text" class="form-control" id="title" placeholder="First Name" value="${obj.title || ''}" required>
       </div>
       <div class="form-group">
-        <label for="image">Last Name</label>
-        <input type="text" class="form-control" id="last_name" placeholder="Last Name" value="${obj.last_name || ''}"required>
+        <label for="image">Definition</label></label>
+        <input type="text" class="form-control" id="definition" placeholder="Definition" value="${obj.definition || ''}"required>
       </div>
       <div class="form-group">
-        <label for="title">Email</label>
-        <input type="email" class="form-control" id="email" aria-describedby="Email" placeholder="Enter Email" value="${obj.email || ''}"required>
+        <label for="title">Language/Tech</label>
+        <input type="email" class="form-control" id="languageTech" aria-describedby="Language/Tech" placeholder="Enter Email" value="${obj.email || ''}"required>
       </div>
-      <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="favorite" ${obj.favorite ? 'checked' : ''}>
-      <label class="form-check-label" for="favorite">Favorite?</label>
-    </div>
-      <button type="submit" class="btn btn-primary mt-3">Submit Author</button>
+      <button type="submit" class="btn btn-primary mt-3">Submit Vocabulary</button>
     </form>`;
 
   renderToDOM('#form-container', domString);
