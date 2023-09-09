68551ca0fef9f2a60605785538b3d63dc6b55fef
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2023-09-09 10:56:56 -0500

    updates

--- pages/viewVocab.js
+++ pages/viewVocab.js
@@ -1,27 +1,29 @@
 import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
 
-const viewVocab = (obj) => {
+const viewVocab = (array) => {
   clearDom();
-
-  const domString = `
+  let domString = '';
+  array.forEach((element) => {
+    domString += `
   <div class="mt-5 d-flex flex-wrap">
    <div class="d-flex flex-column">
      <div class="mt-5">
-       <i id="edit-Vocab-btn--${obj.firebasekey}" class="fas fa-edit btn btn-info"></i>
-       <i id="delete-Vocab--${obj.firebasekey}" class="btn btn-danger fas fa-trash-alt"></i>
+       <i id="edit-Vocab-btn--${element.firebasekey}" class="fas fa-edit btn btn-info"></i>
+       <i id="delete-Vocab--${element.firebasekey}" class="btn btn-danger fas fa-trash-alt"></i>
      </div>
    </div>
    <div class="text-white ms-5 details">
-     <h5>${obj.title} 
+     <h5>${element.title} 
      <hr>
-     <p>${obj.definition || ''}</p> 
+     <p>${element.definition || ''}</p> 
      <hr>    
-     <p>${obj.languageTech || ''}</p> 
+     <p>${element.languageTech || ''}</p> 
       </div>
    </div>`;
 
-  renderToDOM('#view', domString);
+    renderToDOM('#view', domString);
+  });
 };
 
 export default viewVocab;
